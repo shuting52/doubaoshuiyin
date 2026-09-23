558a539c8ababefdafc85d1ff3333fb799dc80c8
shuting52 <shuting52@users.noreply.github.com>
2026-09-23 12:05:05 +0800

    feat: add Android wrapper and release signing support

---
+++ android_wrapper/app/src/main/java/com/example/watermarkhelper/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.watermarkhelper
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.allowFileAccess = true
+        settings.allowContentAccess = true
+        settings.loadsImagesAutomatically = true
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
+        settings.databaseEnabled = true
+        settings.savePassword = false
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
